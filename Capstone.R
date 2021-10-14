8a8f44c97659bea25249e1a07c69137692bcc37c
William Belcher <william.belcher@hotmail.com>
2021-10-14 17:26:14 +1000

    tips

--- Capstone.R
+++ Capstone.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(class)
 library(caret)
 library(e1071)
+library(ROCR)
 
 rm(list=ls())
 importedData <- read.csv(file = "csv_result-Autism-Adult-Data.csv")
@@ -38,11 +39,11 @@ augmentedData$Class.ASD <- as.integer(as.factor(augmentedData$Class.ASD))
 str(augmentedData)
 summary(augmentedData)
 
+
+# 1 = no, 2 = yes
+
 # Age has a maximum value of 383. This is definitely an outlier, should be removed
 augmentedData <- augmentedData[!(augmentedData$id %in% c(53)), ]
-augmentedData <- augmentedData[ , !(names(augmentedData) %in% c('id', 'result'))]
-
-
 
 checked_data <- augmentedData %>%
   rowwise() %>%
@@ -50,10 +51,16 @@ checked_data <- augmentedData %>%
 mismatched_data <- checked_data[checked_data$result != checked_data$question_sum, ]
 
 
+augmentedData <- augmentedData[ , !(names(augmentedData) %in% c('id', 'result'))]
+
 ###############################
 # PCA
 ###############################
-pca.dat <- augmentedData[ , !(names(augmentedData) %in% c('Class.ASD'))]
+pca.dat <- augmentedData[ , !(names(augmentedData) %in% c('Class.ASD', 'ethnicity', 'age', 'gender', 'jundice', 'autism', 'country_of_res', 'used_app_before', 'result', 'age_desc', 'relation'))]
+for(i in 1:10) {
+  pca.dat[ ,i] <- as.integer(pca.dat[ ,i])
+}
+str(pca.dat)
 pr.out <- prcomp(pca.dat, scale = TRUE)
 names(pr.out)
 # column mean
@@ -68,9 +75,10 @@ biplot(pr.out, scale = 0)
 # plot is flipped, so flip it back
 pr.out$rotation <- -pr.out$rotation
 pr.out$x <- -pr.out$x
+dev.new()
 biplot(pr.out, scale = 0)
 autoplot(pr.out, colour = 'gender', shape = FALSE, label.size = 3)
-
+names(pr.out)
 # variance explained by each PC
 pr.var <- pr.out$sdev^2
 # proportion of variance explained
@@ -80,9 +88,10 @@ par(mfrow = c(1,2))
 plot(pve, xlab = "Principal Component", ylab = "Proportion of Variance Explained", ylim = c(0,1), type = "b")
 plot(cumsum(pve), xlab = "Principal Component", ylab = "Cumulative Proportion of Variance Explained", ylim = c(0,1), type = "b")
 
+table(cleanData$A6_Score, cleanData$A9_Score)
 
 ###############################
-# Possible methods: Logistic Regression, KNN, Naive Bayes, 
+# Possible methods: Logistic Regression, KNN, Naive Bayes, Classification Tree, Random Forest
 ###############################
 n <- nrow(augmentedData)
 index <- sample(1:n, n*0.8, replace=FALSE)
@@ -94,14 +103,14 @@ nFolds <- 5
 folds <- createFolds(trainDat, k = nFolds)
 
 
-
+# Not this one
 ###############################
 # KNN
 ###############################
-set.seed(2)
+set.seed(1)
 train.error <- c()
 test.error <- c()
-for(k in 1:30) {
+for(k in 1:50) {
   train.error.temp <- c()
   test.error.temp <- c()
   # % fold cross validation
@@ -116,18 +125,38 @@ for(k in 1:30) {
 }
 # Plotting
 par(mfrow = c(1,1))
-plot(1:30, rowMeans(train.error), col='blue', type='b', ylim = c(0,0.4))
-points(1:30, rowMeans(test.error), col='red', type='b')
-legend(1, 95, legend = c("Training MSE", "Testing MSE"), col = c("blue", "red"), title = "Legend")
+plot(1:50, rowMeans(train.error), col='blue', type='b', ylim = c(0,0.4))
+points(1:50, rowMeans(test.error), col='red', type='b')
+# legend(1, 95, legend = c("Training MSE", "Testing MSE"), col = c("blue", "red"), title = "Legend")
+best_k <- which.min(rowMeans(test.error))
+best_k
 
 # Test set
-model.knn.pred = knn(train=testDat[, -19], test=testDat[, -19], cl=testDat[, 19], k=11)
+model.knn.pred = knn(train=testDat[, -19], test=testDat[, -19], cl=testDat[, 19], k=best_k)
 # checking results
 table(model.knn.pred, testDat[, 19])
-accuracy.k11 = mean(model.knn.pred == testDat[, 19])
-error.rate.k11 = mean(model.knn.pred != testDat[, 19])
-accuracy.k11
-error.rate.k11
+
+model.knn.pred2 = knn(train=testDat[, -19], test=testDat[, -19], cl=testDat[, 19], k=1)
+model.knn.pred3 = knn(train=testDat[, -19], test=testDat[, -19], cl=testDat[, 19], k=5)
+model.knn.pred4 = knn(train=testDat[, -19], test=testDat[, -19], cl=testDat[, 19], k=10)
+
+pred_knn <- prediction(as.numeric(model.knn.pred), testDat[, 19])
+perf <- performance(pred_knn, "tpr", "fpr")
+
+pred_knn2 <- prediction(as.numeric(model.knn.pred2), testDat[, 19])
+perf2 <- performance(pred_knn2, "tpr", "fpr")
+
+pred_knn3 <- prediction(as.numeric(model.knn.pred3), testDat[, 19])
+perf3 <- performance(pred_knn3, "tpr", "fpr")
+
+pred_knn4 <- prediction(as.numeric(model.knn.pred4), testDat[, 19])
+perf4 <- performance(pred_knn4, "tpr", "fpr")
+
+plot(perf, avg="threshold", colorize = TRUE)
+plot(perf2, add = TRUE, avg="threshold", colorize = TRUE)
+plot(perf3, add = TRUE, avg="threshold", colorize = TRUE)
+plot(perf4, add = TRUE, avg="threshold", colorize = TRUE)
+
 
 ###############################
 # Naive Bayes
@@ -135,3 +164,24 @@ error.rate.k11
 train_control <- trainControl(method = "cv", number = 5)
 model.nb <- train(trainDat[, -19], trainDat[, 19], 'nb', trControl = trainControl(method = 'cv', number = 5))
 
+augmentedData <- cleanData
+augmentedData <- augmentedData[!(augmentedData$id %in% c(53)), ]
+augmentedData$age <- as.numeric(augmentedData$age)
+augmentedData <- augmentedData[ , !(names(augmentedData) %in% c('id', 'result'))]
+for(i in 1:10) {
+  augmentedData[ ,i] <- as.factor(augmentedData[ ,i])
+}
+augmentedData$Class.ASD <- as.numeric(as.factor(augmentedData$Class.ASD)) - 1
+#check
+str(augmentedData)
+
+
+
+newData <- cleanData[, c(12:16, 22)]
+newData <- cbind(newData, pr.out$x[, 1:3])
+#newData <- newData[!(newData$id %in% c(53)), ]
+
+LR.model <- glm(Class.ASD ~., family = binomial("logit"), newData)
+summary(LR.model)
+
+table(augmentedData$age)
